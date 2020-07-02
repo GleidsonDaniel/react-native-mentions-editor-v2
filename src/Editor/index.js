737e07bb96f0b6dee99ff1863346a830ea92527d
Gleidson Daniel Silva <gleidson10daniel@hotmail.com>
2020-07-02 09:42:53 -0300

    Remove selection prop

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -595,7 +595,7 @@ export class Editor extends React.Component {
                 value={state.inputText}
                 onBlur={props.toggleEditor}
                 onChangeText={this.onChange}
-                selection={this.state.selection}
+                // selection={this.state.selection}
                 onSelectionChange={this.handleSelectionChange}
                 onContentSizeChange={this.onContentSizeChange}
                 scrollEnabled={false}
